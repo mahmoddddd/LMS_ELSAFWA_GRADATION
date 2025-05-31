d912abdd38103d10892701e475cf5d7449b8cf88
Mhmod Elsherif <90068731+mahmoddddd@users.noreply.github.com>
2025-06-01 00:25:18 +0300

    Add role field to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,18 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  enrolledCourses: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Course'
-    }
-  ],
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    role: { type: String, enum: ["student", "educator"], default: "student" },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
