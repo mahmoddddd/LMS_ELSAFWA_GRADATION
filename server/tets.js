d2a8def552e32e20daa6c9ae5bbf6a391a51ed98
Mhmod Elsherif <90068731+mahmoddddd@users.noreply.github.com>
2025-06-03 07:48:01 +0300

    comit in tets.js

--- server/tets.js
+++ server/tets.js
@@ -2,3 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 console.log('NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:', process.env.CLERK_PUBLISHABLE_KEY);
+// in this commit < all file work till user creat acount and login with no error and 
+// user can create its role to educator and log in again and see educator page and its redirect to dashboard
